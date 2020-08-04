4d713b0bc446ca1128f7ea4b851148c498038550
Tech-Inside <techinnermost@gmail.com>
2020-08-04 11:46:21 +0530

    initial commit

---
+++ camera/main/main.c
@@ -0,0 +1,233 @@
+// /* Hello World Example
+
+//    This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+//    Unless required by applicable law or agreed to in writing, this
+//    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//    CONDITIONS OF ANY KIND, either express or implied.
+// */
+// #include <stdio.h>
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "esp_system.h"
+// #include "esp_spi_flash.h"
+
+
+// void app_main(void)
+// {
+//     printf("Hello world!\n");
+
+//     /* Print chip information */
+//     esp_chip_info_t chip_info;
+//     esp_chip_info(&chip_info);
+//     printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+//             chip_info.cores,
+//             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+//             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+//     printf("silicon revision %d, ", chip_info.revision);
+
+//     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+//             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+//     for (int i = 10; i >= 0; i--) {
+//         printf("Restarting in %d seconds...\n", i);
+//         vTaskDelay(1000 / portTICK_PERIOD_MS);
+//     }
+//     printf("Restarting now.\n");
+//     fflush(stdout);
+//     esp_restart();
+// }
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <esp_event_loop.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/sdmmc_host.h"
+#include "driver/sdmmc_defs.h"
+#include "sdmmc_cmd.h"
+#include "esp_vfs_fat.h"
+
+#include "esp_camera.h"
+
+static const char *TAG = "Camera";
+#define MOUNT_POINT "/sdcard"
+
+#define BOARD_ESP32CAM_AITHINKER 1
+
+// WROVER-KIT PIN Map
+#ifdef BOARD_WROVER_KIT
+
+#define CAM_PIN_PWDN -1  //power down is not used
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 21
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 19
+#define CAM_PIN_D2 18
+#define CAM_PIN_D1 5
+#define CAM_PIN_D0 4
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#endif
+
+// ESP32Cam (AiThinker) PIN Map
+#ifdef BOARD_ESP32CAM_AITHINKER
+
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#endif
+
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+static esp_err_t init_camera()
+{
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+
+static void init_sdcard()
+{
+  esp_err_t ret = ESP_FAIL;
+
+  esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+      .format_if_mount_failed = false,
+      .max_files = 5,
+      .allocation_unit_size = 16 * 1024
+  };
+  sdmmc_card_t *card;
+
+  const char mount_point[] = MOUNT_POINT;
+  ESP_LOGI(TAG, "Initializing SD card");
+
+  sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+  sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+  ESP_LOGI(TAG, "Mounting SD card...");
+  gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
+  gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);    // D0, needed in 4- and 1-line modes
+  gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);    // D1, needed in 4-line mode only
+  gpio_set_pull_mode(12, GPIO_PULLUP_ONLY);   // D2, needed in 4-line mode only
+  gpio_set_pull_mode(13, GPIO_PULLUP_ONLY);   // D3, needed in 4- and 1-line modes
+
+  ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+  if (ret == ESP_OK)
+  {
+    ESP_LOGI(TAG, "SD card mount successfully!");
+  }
+  else
+  {
+    ESP_LOGE(TAG, "Failed to mount SD card VFAT filesystem. Error: %s", esp_err_to_name(ret));
+  }
+
+  // Card has been initialized, print its properties
+  // sdmmc_card_print_info(stdout, card);
+
+}
+
+
+void app_main(void)
+{
+    init_camera();
+    init_sdcard();
+
+    while(1)
+    {
+
+        ESP_LOGI(TAG, "Taking picture...");
+        camera_fb_t *pic = esp_camera_fb_get();
+        static uint64_t counter = 0;
+        counter++;
+
+        char *pic_name = malloc(30 + sizeof(int64_t));
+        sprintf(pic_name, MOUNT_POINT"/pic_%lli.jpg", counter);
+        FILE *file = fopen(pic_name, "w");
+
+        if (file != NULL)
+        {
+            fwrite(pic->buf, 1, pic->len, file);
+            ESP_LOGI(TAG, "File saved: %s", pic_name);
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Could not open file =(");
+        }
+        fclose(file);
+        free(pic_name);
+
+        vTaskDelay(3000 / portTICK_RATE_MS);
+
+    }
+}
